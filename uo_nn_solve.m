4c26414a7f9c968b255be1e24da3fb6dfd5fddc2
JP <jpniznhy@hotmail.com>
2023-03-21 15:22:58 +0100

    Add files via upload

--- uo_nn_solve.m
+++ uo_nn_solve.m
@@ -14,7 +14,7 @@
 %        ils : line search (1 if exact, 2 if uo_BLS, 3 if uo_BLSNW32)
 %     ialmax :  formula for the maximum step lenght (1 or 2).
 %    kmaxBLS : maximum number of iterations of the uo_BLSNW32.
-%      epsal : minimum progress in alpha, algorithm up_BLSNW32
+%      epsal : minimum progress in alpha, algorithm up_BLSNW32.
 %      c1,c2 : (WC) parameters.
 %        isd : optimization algorithm.
 %     sg_al0 : \alpha^{SG}_0.
@@ -38,25 +38,27 @@
 %     te_acc : Accuracy^{TE}.
 %      niter : total number of iterations.
 %        tex : total running time (see "tic" "toc" Matlab commands).
-function [Xtr,ytr,wo,fo,tr_acc,Xte,yte,te_acc,niter,tex]=uo_nn_solve(num_target,tr_freq,tr_seed,tr_p,te_seed,te_q,la,epsG,kmax,ils,ialmax,kmaxBLS,epsal,c1,c2,isd,sg_al0,sg_be,sg_ga,sg_emax,sg_ebest,sg_seed,icg,irc,nu)
+function [Xtr,ytr,wo,fo,tr_acc,Xte,yte,te_acc,niter,tex] = uo_nn_solve(num_target,tr_freq,tr_seed,tr_p,te_seed,te_q,la,epsG,kmax,ils,ialmax,kmaxBLS,epsal,c1,c2,isd,sg_al0,sg_be,sg_ga,sg_emax,sg_ebest,sg_seed,icg,irc,nu)
     %% Generate the training and test datasets
     [Xtr,ytr] = uo_nn_dataset(tr_seed, tr_p, num_target, tr_freq);
     [Xte,yte] = uo_nn_dataset(te_seed, te_q, num_target, 0.0);
     %% Part 1: recognition of some specific target with GM and QNM
     w = zeros(size(Xtr,1),1);                                               %% como defines w?? unos?? ceros --> 100%
     sig = @(X) 1./(1 + exp(-X)); y = @(X,w) sig(w'*sig(X));
-    L = @(w) (norm(y(Xtr,w) - ytr)^2)/size(ytr,2) + (la*norm(w)^2)/2;
+    L = @(w) (norm(y(Xtr,w) - ytr)^2)/size(ytr,2) + (la*norm(w)^2)/2;           %% es en funcion de X y y???
     gL = @(w) (2*sig(Xtr)*((y(Xtr,w) - ytr).*y(Xtr,w).*(1 - y(Xtr,w)))')/size(ytr,2) + la*w;
     
     if isd == 1 || isd == 3
-        [wk,dk,Lk,gLk,alk,iWk,betak,Hk,niter] = uo_nn_solve_opt(w,L,gL,epsG,kmax,ialmax,kmaxBLS,epsal,c1,c2,isd,icg,irc,nu);
+        [wk,dk,Lk,gLk,alk,iWk,betak,Hk,niter] = uo_nn_solve_fdm(w,L,gL,epsG,kmax,ialmax,kmaxBLS,epsal,c1,c2,isd,icg,irc,nu);
+        wo=wk(:,end); fo=Lk(:,end);
     end
     %% Part 2: stochastic gradient method (SGM)
     if isd == 7
-        [wk,Lk,niter] = uo_nn_solve_sgm(w,L,gL,sg_al0,sg_be,sg_ga,sg_emax,sg_ebest,sg_seed);
+        [wo] = uo_nn_solve_sgm(w,L,gL,sg_al0,sg_be,sg_ga,sg_emax,sg_ebest,sg_seed);
+        fo = L(wo);
     end
     %% accuracy and output variables
-    wo=wk(:,end); fo=Lk(:,end);
+    
     
     tr_v = []; te_v = [];
     for j = 1:tr_p tr_v = [tr_v, round(y(Xtr(1:end,j),wo))]; end
@@ -70,23 +72,28 @@ end
 
 
 % [start] FDM Alg. %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [wk,dk,Lk,gLk,alk,ioutk,betak,Hk,k] = uo_nn_solve_opt(w,L,gL,epsG,kmax,ialmax,kmaxBLS,epsal,c1,c2,isd,icg,irc,nu)
+function [wk,dk,Lk,gLk,alk,ioutk,betak,Hk,k] = uo_nn_solve_fdm(w,L,gL,epsG,kmax,ialmax,kmaxBLS,epsal,c1,c2,isd,icg,irc,nu)
     k = 1; n = size(w,1); DC = 1;
     wk = [w]; dk = []; Lk = [L(w)]; gLk = [gL(w)]; alk = []; ioutk = [];
     betak = []; Hk = [];
     while norm(gL(w)) > epsG && k < kmax && DC
+        %% Descent direction
         [d, beta, H] = uo_nn_descent_direction(w,wk,dk,Hk,gL,isd,icg,irc,nu,k,n);
         dk = [dk, d]; betak = [betak, beta]; Hk(:,:,k) = H;
-
-        if k > 1 ialmax = alk(:,k-1)*(gLk(:,k-1)'*dk(:,k-1))/(gL(w)'*d); end  %% probar para min tiempo
-%         if k > 1 ialmax = 2*(L(w)-Lk(:,k-1))/(gL(w)'*d); end
+        %% Step line search
+        if k > 1                                                                      %% probar para min tiempo
+            ialmax = alk(:,k-1)*(gLk(:,k-1)'*dk(:,k-1))/(gL(w)'*d);
+        end
+%         if k > 1
+%             ialmax = 2*(L(w)-Lk(:,k-1))/(gL(w)'*d);
+%         end
         [alpha,iout] = uo_BLSNW32(L,gL,w,d,ialmax,c1,c2,kmaxBLS,epsal);
         alk = [alk, alpha]; ioutk = [ioutk, iout];
-        
+        %% Descent condition
         if gL(w)'*d >= 0; DC = 0; end
-        w = w + alpha*d;
+        %% Update Variables
+        w = w + alpha*d; k = k + 1;
         wk = [wk, w]; Lk = [Lk, L(w)]; gLk = [gLk, gL(w)];
-        k = k+1;
     end
 end
 % [end] FDM Alg. %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -94,19 +101,36 @@ end
 
 
 % [start] Stochastic Gradient Method %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [wk,Lk,niter] = uo_nn_solve_sgm(w,L,gL,sg_al0,sg_be,sg_ga,sg_emax,sg_ebest,sg_seed)
-    p = size(Xtr, 2);
+function [wo] = uo_nn_solve_sgm(w,L,gL,sg_al0,sg_be,sg_ga,sg_emax,sg_ebest,sg_seed)
+    p = size(Xtr, 2); wo = w;
     m = abs(sg_ga*p); ke = ceil(p/m); kmax = sg_emax*ke; e = 0; s = 0; Lbest = Inf; k = 0;
     while e <= sg_emax && s < sg_ebest
-        P = Xtr(:,randsample(1:p,p));
+        P = Xtr(:,randsample(1:p,p));                                       %% se usa?????
+        %% Minibatches
         for i = 0:ceil(p/m-1)
-            S = 
+            S = []; Xtrs = []; ytrs = [];
+            for s = 1:min((i+1)*m,p)
+                S = [S, s]; Xtrs = [Xtrs, Xtr(:,s)]; ytrs = [ytrs, ytr(:,s)];
+            end
+            %% Descent direction
+            d = -gL();                                                     %%%% falta
+            %% Learning rate
+            sg_k = floor(sg_be*kmax); sg_al = 0.01*sg_al0;
+            if k <= sg_k
+                al = (1-k/sg_k)*sg_al0 + (k*sg_al)/sg_k;
+            elseif k > sg_k
+                al = sg_al;
+            w = w + al*d; k = k + 1;
+            end
+        end
+        %% Stopping criteria
+        e = e + 1; 
+        Lte = ;                                                             %%%%%%% falta
+        if Lte < Lbest
+            Lbest = Lte; wo = w; s = 0;
+        else
+            s = s + 1;
         end
     end
 end
 % [end] Stochastic Gradient Method %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
-
-
-%
